a7b278c03452a0059ab3cf7ad7d4565a2edcdc4f
Emma <emmawa@verizon.net>
2024-11-27 16:27:15 -0500

    Delete assets directory

--- assets/js/header-menu.js
+++
@@ -1,6 +0,0 @@
-let menubutton = document.querySelector('#menubutton');
-let siteheader = document.querySelector('.site-header');
-
-menubutton.addEventListener('click', () => {
-  siteheader.classList.toggle('menu-panel-open');
-});
